b2691bbc75d7b4092d669eacfdf34c84c915e69c
Ryan Kirk <ryanlkirk@live.co.uk>
2023-06-05 20:41:13 +0100

    Add basic blackjack logic

--- src/Helpers/CardHelpers.tsx
+++ src/Helpers/CardHelpers.tsx
@@ -3,3 +3,14 @@ export const getDeckColour = (card: string | undefined ) => {
     defaultSuit = card && (card.at(-1) === '♠' || card.at(-1) === '♣') ? 'redSuit' : 'blackSuit';
     return defaultSuit;
 };
+
+export const getNumericalValue = (card: string) => {
+    const cardVal = parseInt(card.slice(0,1));
+    if( card.slice(0,1) === "A") {
+        return 11;
+    } else if( !cardVal ) {
+        return 10;
+    } else {
+        return cardVal;
+    }
+}
